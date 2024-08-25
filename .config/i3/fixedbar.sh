336cf8fca0a6829e36b879c9103df0b6ac00c155
fixedbyte <wilmerquispecode@gmail.com>
2024-08-25 00:25:13 -0500

    edit status bar, learning sh

--- .config/i3/fixedbar.sh
+++ .config/i3/fixedbar.sh
@@ -1,4 +1,38 @@
 #!/bin/sh
+# Variables
+
+# Functions
+function common {
+    echo -n "\"separator\":false,"
+    echo -n "\"separator_block_width\":0,"
+    echo -n "\"border_top\":0,"
+    echo -n "\"border_bottom\":0,"
+    echo -n "\"border_right\":0,"
+    echo -n "\"border_left\":0"
+}
+
+function colon {
+    echo -n ","
+}
+
+function separator {
+    echo -n "{"
+    echo -n "\"full_text\":\"\","
+    echo -n "\"color\":\"#ff0000\","
+    common
+    echo -n "}"
+    colon
+}
+
+function get_date {
+    separator
+    echo -n "{"
+    echo -n "\"name\":\"time\","
+    echo -n "\"full_text\":\" $(date "+%a %D %T") \","
+    echo -n "\"background\":\"#ff0000\","
+    common
+    echo -n "}"
+}
 
 # Header that will tell i3bar what JSON version will use:
 echo '{ "version": 1, "click_events": true }'
@@ -6,30 +40,16 @@ echo '{ "version": 1, "click_events": true }'
 echo '['
 # Start the loop simpler
 echo '[]'
-# Infinite loop, calls every second the system command _date_
+# Infinite loop
 while :; do
-    echo ",["
-    echo -n "{"
-    echo -n "\"full_text\":\"\","
-    echo -n "\"border_left\":0,"
-    echo -n "\"border_right\":0,"
-    echo -n "\"border_top\":0,"
-    echo -n "\"border_bottom\":0,"
-    echo -n "\"color\":\"#ff0000\","
-    echo -n "\"separator\": false,"
-    echo -n "\"separator_block_width\": 0"
-    echo -n "},"
-    echo -n "{"
-    echo -n "\"name\": \"time\","
-    echo -n "\"full_text\": \"$(date)\","
-    echo -n "\"background\": \"#ff0000\","
-    echo -n "\"border_top\":2,"
-    echo -n "\"border_bottom\":2,"
-    echo -n "\"border_right\":0,"
-    echo -n "\"border_left\":0,"
-    echo -n "\"separator\": false,"
-    echo -n "\"separator_block_width\": 0"
-    echo -n "}"
-    echo "]"
+    echo -n ",["
+    get_date
+#    separator
+    echo -n "]"
     sleep 1
+done &
+while read line; do
+    if [[ $line == *"name"*"time"* ]]; then
+        alacritty --class calendar_event -e $HOME/.config/i3/calendar_event.sh
+    fi
 done
